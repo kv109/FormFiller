63120de7fdfdde39c01b50a63998ddae2e3f5416
Kacper Walanus <kacper@walanus.com>
2012-11-28 16:20:51 +0100

    sync prototype

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-
 _AL_CONF = {
     version : '1.0.0',
+    storage: chrome.storage.sync,
     constants: {
         types: ['login', 'password', 'email', 'name', 'last_name', 'phone', 'street', 'city'],
         innerFormClass: '.innerForm',
@@ -10,6 +10,67 @@ _AL_CONF = {
             options: '_AL_OPTIONS',
             optionsNumber: '_AL_OPTIONS_NUMBER'
         }
+    },
+    
+    sync: {
+        send: function(changes) {
+            var hash = {}
+            hash[_AL_CONF.alValues.lsKey()] = changes;
+            hash[_AL_CONF.encKey.lsKey()]   = _AL_CONF.encKey.get();
+            
+            chrome.storage.sync.set(hash, function() {
+            });
+
+        },
+        receive: function(changes) {
+            var alValues = changes[_AL_CONF.alValues.lsKey()];
+            var encKey = changes[_AL_CONF.encKey.lsKey()];
+            
+            if(alValues) {
+                _AL_CONF.alValues.set(alValues, false);
+            }
+            
+            if(encKey) {
+                _AL_CONF.encKey.set(encKey, false);
+            }
+        },
+        
+        get: function() {
+            _AL_CONF.storage.get(_AL_CONF.alValues.lsKey(), function(changes){
+                var alValues = changes[_AL_CONF.alValues.lsKey()];
+                _AL_CONF.storage.get(_AL_CONF.encKey.lsKey(), function(changes){
+                    var encKey = changes[_AL_CONF.encKey.lsKey()];
+		    var hash = {}
+                    hash[_AL_CONF.encKey.lsKey()] = encKey;
+		    hash[_AL_CONF.alValues.lsKey()] = alValues;
+		    _AL_CONF.sync.receive(hash);
+                })
+            })
+        },
+
+	changesToHash: function(changes) {
+            var hash = {}
+            var alValues = changes[_AL_CONF.alValues.lsKey()];
+            if(typeof(alValues) != 'undefined') {
+                alValues = alValues.newValue;
+            }
+	    else {
+	        alValues = null;
+	    }
+
+            var encKey = changes[_AL_CONF.encKey.lsKey()];
+            if(typeof(encKey) != 'undefined') {
+                encKey = encKey.newValue;
+            }
+	    else {
+	        encKey = null;
+	    }
+
+            hash[_AL_CONF.alValues.lsKey()] = alValues;
+            hash[_AL_CONF.encKey.lsKey()]   = encKey;
+
+	    return hash;
+	}
     },
 
     options: {
@@ -62,6 +123,17 @@ _AL_CONF = {
                 _AL_CONF.options.setDefault();
             }
         },
+        
+        setEvents: function() {
+            chrome.storage.onChanged.addListener(function(changes, namespace) {
+		var changeHash = _AL_CONF.sync.changesToHash(changes);
+                _AL_CONF.sync.receive(changeHash);                
+            });
+        },
+        
+        synchronizeConf: function() {
+            _AL_CONF.sync.get();
+        },
 
         start: function() {
             var _init = _AL_CONF.initialize;
@@ -69,6 +141,8 @@ _AL_CONF = {
             _init.setLocalStorage.optionsNumber();
             _init.setTranslations();
             _init.setDefaultOptions();
+            _init.synchronizeConf();
+            _init.setEvents();
         }
     },
 
@@ -85,8 +159,12 @@ _AL_CONF = {
         get: function() {
             return fromStorage(_AL_CONF.alValues.lsKey());
         },
-        set: function(alValues) {
-            return toStorage(_AL_CONF.alValues.lsKey(), alValues);
+        set: function(alValues, sync) {
+            var key = _AL_CONF.alValues.lsKey();
+            if(sync!=false) {
+                _AL_CONF.sync.send(alValues);
+            }
+            toStorage(key, alValues);
         }
     },
 
@@ -94,8 +172,14 @@ _AL_CONF = {
         get: function() {
             return fromStorage(_AL_CONF.constants.localStorageKeys.encKey)
         },
-        set: function() {
-            toStorage(_AL_CONF.constants.localStorageKeys.encKey, randomString())
+        set: function(key) {
+            if(typeof(key)=='undefined') {
+                key = randomString();
+            }
+            toStorage(_AL_CONF.constants.localStorageKeys.encKey, key)
+        },
+        lsKey: function() {
+            return _AL_CONF.constants.localStorageKeys.encKey;
         }
     },
 
