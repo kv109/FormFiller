0928d3d22afaee5396cb3e873a0f32a234f189e1
Kacper Walanus <kacper@walanus.com>
2012-12-06 15:00:59 +0100

    constants.js

---
+++ constants.js
@@ -0,0 +1,7 @@
+TYPES = ['login', 'password', 'email', 'name', 'last_name', 'phone', 'street', 'city'];
+LOCAL_STORAGE_KEYS = {
+	alValues:      '_AL_VALUES',
+	encKey:        '_AL_ENC_KEY',
+	options:       '_AL_OPTIONS',
+	optionsNumber: '_AL_OPTIONS_NUMBER'
+}
