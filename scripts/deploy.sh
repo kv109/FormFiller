11f2b819842a8da4d12f5f1044e5dcd07ee9632a
Kacper Walanus <kacper@walanus.com>
2012-12-11 22:05:31 +0100

    sed in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,11 @@
+version=$1
+echo "Version $version will be created."
 cd ../..
 rm -rf Form-Filler-Deploy
 cp -r Form-Filler Form-Filler-Deploy
 cd Form-Filler-Deploy
+sed -i -e's/"version.*[0-9\.]"/"version": "'$version'"/' manifest.json		#version
+sed -i -e's/[^:]\/\/.*//' manifest.json		#remove comments
 rm -rf .git
 rm .gitignore
 rm -rf nbproject
@@ -11,5 +15,3 @@ rm *.js
 cd ..
 cd scripts
 ./bundle_scripts.sh
-cd ../..
-tar -czf formfiller.zip Form-Filler-Deploy/
