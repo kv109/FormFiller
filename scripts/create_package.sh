b3021cada98df3e037690f9c7c0a6f9bcc6095bf
Kacper Walanus <kacper@walanus.com>
2012-12-11 22:13:49 +0100

    remove "key" in manifest.json when create package

--- scripts/create_package.sh
+++ scripts/create_package.sh
@@ -6,6 +6,7 @@ cp -r Form-Filler Form-Filler-Deploy
 cd Form-Filler-Deploy
 sed -i -e's/"version.*[0-9\.]"/"version": "'$version'"/' manifest.json		#version
 sed -i -e's/[^:]\/\/.*//' manifest.json		#remove comments
+sed -i -e's/"key.*//' manifest.json		#remove line with "key"
 rm -rf .git
 rm .gitignore
 rm -rf nbproject
